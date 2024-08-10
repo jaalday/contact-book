8514b683a6f3f94f1674e6945dc136ee194cc46e
Jaclyn Alday <148822253+jaalday@users.noreply.github.com>
2024-08-10 15:14:53 -0400

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,7 +3,7 @@ export default function Index() {
       <p id="zero-state">
         <img src="https://cdn-icons-png.freepik.com/512/211/211064.png" alt="yellow phone book" />
         <p>Add Contacts to the search bar on the left!</p>
-        <p>This was a Tutorial from React Router</p>
+      
       </p>
     );
   }
